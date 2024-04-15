eeeb861d6c6f5026d2f3a64656cdd30647e11609
The Premelar <premelar7@gmail.com>
2024-04-15 17:44:55 +0700

    New Style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1.0",
-      title:"Money W.N.C"
+      title: "Money W.N.C"
     }
   },
   devtools: { enabled: true },
