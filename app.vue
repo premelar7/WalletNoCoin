50be837e7b041830f6c85079543b4f921edbeb0c
The Premelar <premelar7@gmail.com>
2024-04-15 17:59:58 +0700

    New Style

--- app.vue
+++ app.vue
@@ -3,17 +3,3 @@
     <NuxtPage />
   </div>
 </template>
-
-<script setup lang="ts">
-useHead({
-  meta: [
-    { name: 'apple-mobile-web-app-capable', content: 'yes' },
-    { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
-    { name: 'apple-mobile-web-app-title', content: 'App Title' },
-  ],
-  link: [
-    { rel: "apple-touch-icon", href: "portal.kirimsms.id/_nuxt/img/kirimLogoX2.8fa05d8.png" },
-    { rel: "apple-touch-startup-image", href: "portal.kirimsms.id/_nuxt/img/kirimLogoX2.8fa05d8.png" }
-  ]
-})
-</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,20 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
     head: {
-      charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1.0",
-      title: "Money W.N.C"
+      title: "Money W.N.C",
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+        { name: 'apple-mobile-web-app-title', content: 'WNC Money' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icons/title.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/title.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/icons/title.png' },
+        { rel: 'apple-touch-iconapple-touch-startup-image', href: '/icons/title.png' }
+      ]
     }
   },
   devtools: { enabled: true },
