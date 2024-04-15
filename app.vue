164a12f4455ec0437d658de1bcd35b9299e86da2
The Premelar <premelar7@gmail.com>
2024-04-15 17:40:54 +0700

    New Title

--- app.vue
+++ app.vue
@@ -1,26 +1,19 @@
 <template>
   <div>
-
-    <Head>
-      <Title>wnc : my money</Title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta name="apple-mobile-web-app-capable" content="yes">
-      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-      <meta name="apple-mobile-web-app-title" content="App Title">
-      <link rel="apple-touch-icon" href="portal.kirimsms.id/_nuxt/img/kirimLogoX2.8fa05d8.png">
-      <link rel="apple-touch-startup-image" href="portal.kirimsms.id/_nuxt/img/kirimLogoX2.8fa05d8.png">
-    </Head>
     <NuxtPage />
   </div>
 </template>
 
 <script setup lang="ts">
-// useSeoMeta({
-//   title: 'My Amazing Site',
-//   ogTitle: 'My Amazing Site',
-//   description: 'This is my amazing site, let me tell you all about it.',
-//   ogDescription: 'This is my amazing site, let me tell you all about it.',
-//   ogImage: 'https://example.com/image.png',
-//   twitterCard: 'summary_large_image',
-// })
+useHead({
+  meta: [
+    { name: 'apple-mobile-web-app-capable', content: 'yes' },
+    { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+    { name: 'apple-mobile-web-app-title', content: 'App Title' },
+  ],
+  link: [
+    { rel: "apple-touch-icon", href: "portal.kirimsms.id/_nuxt/img/kirimLogoX2.8fa05d8.png" },
+    { rel: "apple-touch-startup-image", href: "portal.kirimsms.id/_nuxt/img/kirimLogoX2.8fa05d8.png" }
+  ]
+})
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1.0",
+      title:"Money W.N.C"
+    }
+  },
   devtools: { enabled: true },
   vite: {
     css: {
