05eba43049e0eaa17760d3e1996e91a23601fb7e
The Premelar <premelar7@gmail.com>
2024-04-15 16:31:31 +0700

    Marker deploy

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <div style="text-align: center; width: 100%; padding-top: 10px; font-weight: bold;">ทดสอบการ deploy the new project
+    </div>
     <NuxtWelcome />
   </div>
 </template>
